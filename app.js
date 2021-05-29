8d1f0d37e22c5826dfae4ff6852d9b52324631e5
RishikShr <48129871+RishikShr@users.noreply.github.com>
2021-05-29 18:17:22 +0530

    [Be]-added cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express  = require('express');
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
- const PORT  = process.env.PORT || 6000;;
+const PORT  = process.env.PORT || 6000;;
 const {MONGOURI} = require('./keys')
 
 require('./models/user')
 
+app.use(cors())
 app.use(express.json())
 app.get('/home',(req,res)=>{
     res.send('hello world')
